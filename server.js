b77421a97f1ce84ce7bb9181fe3d37dfbe7dee0a
Tahir <Tahirhussain32145@gmail.com>
2022-07-12 07:59:43 +0545

    before data changes

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const port = process.env.port || process.env.port || 5000;
+const express = require("express");
+const port = process.env.port || process.env.port || 8000;
 
 const app = express();
 
-
 //add the college information here in the same format
-var collegesInformations = [ 
-    {"id":1,"name":"collegeName", "location":"collegeLocation", "contact":"collegeContact"}, 
-    {"id":2,"name":"collegeName", "location":"collegeLocation2", "contact":"collegeContact2"}, 
-    {"id":3,"name":"collegeName", "location":"collegeLocation3", "contact":"collegeContact3"}, 
- ];
+var collegesInformations = [
+  {
+    id: 1,
+    name: "collegeName",
+    location: "collegeLocation",
+    contact: "collegeContact",
+  },
+  {
+    id: 2,
+    name: "collegeName",
+    location: "collegeLocation2",
+    contact: "collegeContact2",
+  },
+  {
+    id: 3,
+    name: "collegeName",
+    location: "collegeLocation3",
+    contact: "collegeContact3",
+  },
+];
 
-app.get('/collegeinfo', (req,res)=> {
-    res.status(200).send(collegesInformations);
-})
+app.get("/collegeinfo", (req, res) => {
+  res.status(200).send(collegesInformations);
+});
 
-app.listen(port, ()=>{
-    console.log('running!');
+app.listen(port, () => {
+  console.log("running!");
 });
